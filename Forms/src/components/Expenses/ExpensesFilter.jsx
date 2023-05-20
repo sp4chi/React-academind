c69d27f48749a346f5f4283c6d5b6e24b32e758a
kaushik.bora <herock76@gmail.com>
2023-05-20 16:14:34 +0530

    -setting value in ExpensesFilter for two way binding

--- Forms/src/components/Expenses/ExpensesFilter.jsx
+++ Forms/src/components/Expenses/ExpensesFilter.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import './ExpensesFilter.css';
 
@@ -14,7 +14,7 @@ const ExpensesFilter = (props) => {
         <div className='expenses-filter'>
             <div className='expenses-filter__control'>
                 <label>Filter by year</label>
-                <select onChange={onSelectHandler}>
+                <select value={props.setDefaultYear} onChange={onSelectHandler}>
                     <option value='2022'>2022</option>
                     <option value='2021'>2021</option>
                     <option value='2020'>2020</option>
