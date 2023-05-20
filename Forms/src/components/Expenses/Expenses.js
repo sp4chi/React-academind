2711a0bc2e4922a7429b0da51bfbe5fa75488d5d
kaushik.bora <herock76@gmail.com>
2023-05-20 21:59:01 +0530

    -rendered ExpenseItem using map

--- Forms/src/components/Expenses/Expenses.js
+++ Forms/src/components/Expenses/Expenses.js
@@ -16,27 +16,17 @@ const Expenses = (props) => {
       <ExpensesFilter setDefaultYear={year} year={getYearHandler} />
 
       <Card >
-
-        <ExpenseItem
-          title={props.items[0].title}
-          amount={props.items[0].amount}
-          date={props.items[0].date}
-        />
-        <ExpenseItem
-          title={props.items[1].title}
-          amount={props.items[1].amount}
-          date={props.items[1].date}
-        />
-        <ExpenseItem
-          title={props.items[2].title}
-          amount={props.items[2].amount}
-          date={props.items[2].date}
-        />
-        <ExpenseItem
-          title={props.items[3].title}
-          amount={props.items[3].amount}
-          date={props.items[3].date}
-        />
+        {props.items.map((item, index) => {
+          //console.log(index)
+          return (
+            <ExpenseItem
+              key={index}
+              title={item.title}
+              amount={item.amount}
+              date={item.date}
+            />
+          )
+        })}
       </Card>
     </div>
   );
