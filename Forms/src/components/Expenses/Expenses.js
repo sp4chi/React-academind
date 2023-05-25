21ff8673454ad581afd519a55c76b4a52093557a
kaushik.bora <herock76@gmail.com>
2023-05-25 15:10:00 +0530

    -added ExpensesList and moved expenseContent contents to ExpensesList components

--- Forms/src/components/Expenses/Expenses.js
+++ Forms/src/components/Expenses/Expenses.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
 import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
+import ExpensesList from './ExpensesList';
 
 const Expenses = (props) => {
   const [year, setYear] = useState('2020');
@@ -12,32 +12,16 @@ const Expenses = (props) => {
 
   const filteredItems = props.items.filter((item) => item.date.getFullYear().toString() === year)
 
-
-  let expenseContent = <p className='no-expense-found'>No expense found</p>
-
-  if (filteredItems.length > 0) {
-    expenseContent = (filteredItems.map((item, index) => {
-      return (
-        <ExpenseItem
-          key={item.id}
-          title={item.title}
-          amount={item.amount}
-          date={item.date}
-        />
-      )
-    }))
-  }
   return (
-    <div className="expenses">
+    <ul className="expenses">
       <ExpensesFilter
         setDefaultYear={year}
         year={getYearHandler}
       />
-
       <Card >
-        {expenseContent}
+        <ExpensesList items={filteredItems} />
       </Card>
-    </div>
+    </ul>
   );
 }
 
