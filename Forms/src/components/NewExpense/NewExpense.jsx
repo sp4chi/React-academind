73e1c9662ea650c5b5f85c6403be1a10d337452e
kaushik.bora <herock76@gmail.com>
2023-05-25 18:03:45 +0530

    -added new edit state, add new expense button and conditionals in JSX

--- Forms/src/components/NewExpense/NewExpense.jsx
+++ Forms/src/components/NewExpense/NewExpense.jsx
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './NewExpense.css'
 
 import ExpenseForm from './ExpenseForm';
 const NewExpense = (props) => {
+    const [isEditing, setIsEditing] = useState(false);
+    const addNewExpenseHandler = () => {
+        setIsEditing(true);
+    }
 
     const onSaveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString(),
         }
-        //console.log('data from NewExpense: ', expenseData);
+        setIsEditing(false)
         props.onAddExpense(expenseData);
     };
-    return (<div className='new-expense'>
-        <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler} />
-    </div>);
+    const onCancelHandler = () => {
+        setIsEditing(false);
+    };
+
+    return (
+        <div className='new-expense'>
+            {isEditing &&
+                <ExpenseForm
+                    onCancel={onCancelHandler}
+                    onSaveExpenseData={onSaveExpenseDataHandler}
+
+                />}
+            {!isEditing &&
+                <button
+                    onClick={addNewExpenseHandler}
+                >
+                    Add new Expense
+                </button>
+            }
+
+        </div>);
 }
 
 export default NewExpense;
