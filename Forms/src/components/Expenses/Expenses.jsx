5aafeb5bb8d17e4b3bd01462b7b7e638bdb094fd
kaushik.bora <herock76@gmail.com>
2023-05-26 00:04:18 +0530

    -added ExpensesChart component

--- Forms/src/components/Expenses/Expenses.jsx
+++ Forms/src/components/Expenses/Expenses.jsx
@@ -3,6 +3,7 @@ import Card from '../UI/Card';
 import './Expenses.css';
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 
 const Expenses = (props) => {
   const [year, setYear] = useState('2020');
@@ -12,12 +13,14 @@ const Expenses = (props) => {
 
   const filteredItems = props.items.filter((item) => item.date.getFullYear().toString() === year)
 
+  //console.log(filteredItems[0].date.getFullYear().toString());
   return (
     <ul className="expenses">
       <ExpensesFilter
         setDefaultYear={year}
         year={getYearHandler}
       />
+      <ExpensesChart expenses={filteredItems} />
       <Card >
         <ExpensesList items={filteredItems} />
       </Card>
