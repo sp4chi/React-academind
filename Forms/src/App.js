5367c14faef32ed092b04d9d620e0ed18dc2c3f6
kaushik.bora <herock76@gmail.com>
2023-05-25 23:57:25 +0530

    -minor change

--- Forms/src/App.js
+++ Forms/src/App.js
@@ -10,12 +10,17 @@ const DUMMY_DATA = [
     amount: 94.12,
     date: new Date(2020, 7, 14),
   },
-  { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12) },
+  {
+    id: 'e2',
+    title: 'New TV',
+    amount: 799.49,
+    date: new Date(2021, 2, 12)
+  },
   {
     id: 'e3',
     title: 'Car Insurance',
     amount: 294.67,
-    date: new Date(2021, 2, 28),
+    date: new Date(2021, 3, 28),
   },
   {
     id: 'e4',
