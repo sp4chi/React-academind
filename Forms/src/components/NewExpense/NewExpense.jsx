79008fdf75614900a8743b9ffc5d3c5ad4053ed0
kaushik.bora <herock76@gmail.com>
2023-05-25 13:06:26 +0530

    used onAddExpense function to lift up the current state of expense list

--- Forms/src/components/NewExpense/NewExpense.jsx
+++ Forms/src/components/NewExpense/NewExpense.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import './NewExpense.css'
 
 import ExpenseForm from './ExpenseForm';
-const NewExpense = () => {
+const NewExpense = (props) => {
 
     const onSaveExpenseDataHandler = (enteredExpenseData) => {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString(),
         }
-        console.log('data from NewExpense: ', expenseData);
+        //console.log('data from NewExpense: ', expenseData);
+        props.onAddExpense(expenseData);
     };
     return (<div className='new-expense'>
         <ExpenseForm onSaveExpenseData={onSaveExpenseDataHandler} />
