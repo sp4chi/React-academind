31d1226725a74f34b13a1640e127bc5a7d8de3da
kaushik.bora <herock76@gmail.com>
2023-06-12 04:16:11 +0530

    -tapping into cartcontext to get total items

--- Food-Order-App/src/components/Layout/HeaderCartButton.js
+++ Food-Order-App/src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,19 @@
+import { useContext } from 'react';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
+import CartContext from '../../store/cart-context';
 
 const HeaderCartButton = (props) => {
+    const cartCtx = useContext(CartContext);
+
+    const numberOfCartCartItems = cartCtx.items.reduce((currNum, item) => { return currNum + item.amount }, 0)
     return (
         <button onClick={props.onClick} className={classes['button']}>
             <span className={classes['icon']}>
                 <CartIcon />
             </span>
             <span>Your Cart</span>
-            <span className={classes['badge']}>3</span>
+            <span className={classes['badge']}>{numberOfCartCartItems}</span>
         </button>
     );
 }
