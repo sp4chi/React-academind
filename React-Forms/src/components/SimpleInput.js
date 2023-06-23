23d0903cee0bce8ee5258a193a6363cdf750c65f
kaushik.bora <herock76@gmail.com>
2023-06-24 04:58:13 +0530

    -reading inputs with state and ref

--- React-Forms/src/components/SimpleInput.js
+++ React-Forms/src/components/SimpleInput.js
@@ -1,12 +1,35 @@
+import React, { useState, useRef } from 'react';
+
 const SimpleInput = (props) => {
+  const [enteredName, setEnteredName] = useState('');
+
+  const nameInputRef = useRef();
+
+  const nameInputChangeHandler = (e) => {
+    //setEnteredName(e.target.value);
+
+  };
+
+  const formSubmissionHandler = (e) => {
+    e.preventDefault();
+
+    //setEnteredName('')
+    const enteredValue = nameInputRef.current.value;
+    console.log(enteredValue);
+    nameInputRef.current.value = '';
+  };
   return (
-    <form>
+    <form onSubmit={formSubmissionHandler}>
       <div className='form-control'>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input
+          ref={nameInputRef}
+          onChange={nameInputChangeHandler} type='text'
+          id='name'
+        />
       </div>
       <div className="form-actions">
-        <button>Submit</button>
+        <button >Submit</button>
       </div>
     </form>
   );
