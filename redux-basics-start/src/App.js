797bd781eacd1c4322474a665ec64839ae901f23
kaushik.bora <herock76@gmail.com>
2023-07-01 05:20:07 +0530

    -tapping into store from multiple components

--- redux-basics-start/src/App.js
+++ redux-basics-start/src/App.js
@@ -2,14 +2,21 @@ import { Fragment } from 'react';
 import Header from './components/Header'
 import Auth from './components/Auth'
 import Counter from './components/Counter';
+import { useSelector } from 'react-redux';
+import UserProfile from './components/UserProfile';
 
 
 function App() {
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+
+
+
   return (
     <Fragment>
       <Header />
-      <Auth />
-      <Counter />
+      {!isAuthenticated && <Auth />}
+      {isAuthenticated && <UserProfile />}
+      {isAuthenticated && <Counter />}
     </Fragment>
   );
 }
