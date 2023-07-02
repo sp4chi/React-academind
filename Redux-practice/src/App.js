3cfeb883b7c0a2a1474c1f62e0cc4b2f62c5d502
kaushik.bora <herock76@gmail.com>
2023-07-02 06:13:05 +0530

    -sending our cart to firebase

--- Redux-practice/src/App.js
+++ Redux-practice/src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
@@ -5,6 +6,14 @@ import { useSelector } from 'react-redux';
 
 function App() {
   const showCart = useSelector(state => state.ui.showCart);
+  const cart = useSelector(state => state.cart);
+
+  useEffect(() => {
+    fetch('https://react-shopping-cart-de08a-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json', {
+      method: 'PUT',//override existing data with incoming data
+      body: JSON.stringify(cart)
+    });
+  }, [cart]);
   return (
     <Layout>
       {showCart && <Cart />}
