b77a4da83e76d5fa555346b9978906fa8b478992
kaushik.bora <herock76@gmail.com>
2023-07-04 04:14:23 +0530

    -navigating programatically

--- Routing/src/pages/Home.js
+++ Routing/src/pages/Home.js
@@ -1,11 +1,19 @@
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Home = () => {
+  const navigate = useNavigate();
+  const navigateHandler = () => {
+    navigate('/products');
+  };
+
   return (
     <>
       <h1>My home page!</h1>
       <p>
         Go to the list of <Link to="/products">products</Link>
+      </p>
+      <p>
+        <button onClick={navigateHandler}>Navigate</button>
       </p>
     </>
   );
