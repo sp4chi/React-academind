bd353ec309ba57019e8e8fac3fed28b14d468df2
kaushik.bora <herock76@gmail.com>
2023-07-06 03:08:35 +0530

    -added relative attribute for path routing

--- Routing/src/pages/ProductDetail.js
+++ Routing/src/pages/ProductDetail.js
@@ -1,4 +1,4 @@
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 
 const ProductDetail = () => {
   const params = useParams();
@@ -8,6 +8,11 @@ const ProductDetail = () => {
     <>
       <h1>Product Details</h1>
       <p>{params.productId}</p>
+      <p>
+        <Link to=".." relative="path">
+          Back
+        </Link>
+      </p>
     </>
   );
 };
