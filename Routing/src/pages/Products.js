c1c5571d6ff22fdfba2938982c97ac8b5beb31c2
kaushik.bora <herock76@gmail.com>
2023-07-05 15:15:05 +0530

    -products titles are displayed

--- Routing/src/pages/Products.js
+++ Routing/src/pages/Products.js
@@ -1,10 +1,10 @@
 import { Link } from 'react-router-dom';
 
 const PRODUCTS = [
-  { id: 'p1', title: 'Product 1' },
-  { id: 'p2', title: 'Product 2' },
-  { id: 'p3', title: 'Product 3' },
-  { id: 'p4', title: 'Product 4' },
+  { id: 'p1', title: 'Shirts' },
+  { id: 'p2', title: 'Jeans' },
+  { id: 'p3', title: 'Tracks' },
+  { id: 'p4', title: 'Jackets' },
 ];
 const Products = () => {
   return (
@@ -13,7 +13,7 @@ const Products = () => {
       <ul>
         {PRODUCTS.map((product) => (
           <li key={product.id}>
-            <Link to={`/products/${product.id}`}>{product.id}</Link>
+            <Link to={`/products/${product.id}`}>{product.title}</Link>
           </li>
         ))}
       </ul>
