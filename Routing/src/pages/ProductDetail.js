51a5e6a8573d71b96ce66e9065744d8b37829972
kaushik.bora <herock76@gmail.com>
2023-07-05 15:15:30 +0530

    -removed commented console log

--- Routing/src/pages/ProductDetail.js
+++ Routing/src/pages/ProductDetail.js
@@ -2,7 +2,7 @@ import { useParams } from 'react-router-dom';
 
 const ProductDetail = () => {
   const params = useParams();
-  //   console.log(params);
+  console.log(params);
 
   return (
     <>
