708cf2e16de0f76516c2d3dc68fcee1f8ca6a9a5
kaushik.bora <herock76@gmail.com>
2023-07-06 20:58:05 +0530

    -made ExpenseItem stateless

--- Expense-List/src/components/Expenses/ExpenseItem.jsx
+++ Expense-List/src/components/Expenses/ExpenseItem.jsx
@@ -1,32 +1,18 @@
-import React from 'react';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 
 const ExpenseItem = (props) => {
-  let text = props.title;
-
-  const [title, setTitle] = useState(text)
-
-
-  const onClickHandler = () => {
-
-    setTitle('Updated!')
-    console.log(title);
-    //console.log('Clicked');
-  }
-
 
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button onClick={onClickHandler}>Click ME !</button>
     </Card>
   );
 }
