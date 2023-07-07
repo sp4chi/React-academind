d464b983df007d28ac655693675b57fe6b568b8e
kaushik.bora <herock76@gmail.com>
2023-07-07 06:42:08 +0530

    -loader data can be used anywhere we want

--- React-Router-Project/frontend/src/components/EventsList.js
+++ React-Router-Project/frontend/src/components/EventsList.js
@@ -1,14 +1,14 @@
 import { useLoaderData } from 'react-router-dom';
 import classes from './EventsList.module.css';
 
-function EventsList(props) {
-  //const event = useLoaderData();
+function EventsList() {
+  const events = useLoaderData();
 
   return (
     <div className={classes.events}>
       <h1>All Events</h1>
       <ul className={classes.list}>
-        {props.events.map((event) => (
+        {events.map((event) => (
           <li key={event.id} className={classes.item}>
             <a href="...">
               <img src={event.image} alt={event.title} />
