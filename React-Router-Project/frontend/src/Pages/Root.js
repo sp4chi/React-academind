3befd34675e07e01dc94f012d8a3af06e48e3e8e
kaushik.bora <herock76@gmail.com>
2023-07-07 07:21:13 +0530

    -adding a loading indicator

--- React-Router-Project/frontend/src/Pages/Root.js
+++ React-Router-Project/frontend/src/Pages/Root.js
@@ -1,7 +1,22 @@
 import MainNavigation from '../components/MainNavigation';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useNavigation } from 'react-router-dom';
 
 const Root = () => {
+  const navigation = useNavigation();
+
+  if (navigation.state === 'loading') {
+    return (
+      <p
+        style={{
+          display: 'flex',
+          justifyContent: 'center',
+        }}
+      >
+        Loading....
+      </p>
+    );
+  }
+
   return (
     <>
       <MainNavigation />
